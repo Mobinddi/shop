69d8a140a5d86c69b0fc25ae45cf307dba264d70
mobin <mobin.dousttalab@gmail.com>
2022-06-23 18:17:45 +0430

    add crispy forms and bootstrap

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+
     'accounts',
     'pages',
 ]
@@ -129,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
